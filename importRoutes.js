7122670bf77d82c5861e8a1e3298217eddddc90c
Pravallikag123 <166217778+Pravallikag123@users.noreply.github.com>
2025-03-03 11:50:53 +0530

    Add files via upload

---
+++ importRoutes.js
@@ -0,0 +1,10 @@
+// routes/importRoutes.js
+const express = require('express')
+const router = express.Router()
+
+// Define routes here, for example:
+router.post('/import', (req, res) => {
+  res.send('Import route')
+})
+
+module.exports = router

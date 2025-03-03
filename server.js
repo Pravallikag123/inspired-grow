310f36d027e417bac49a0bb2c118ae378cb97143
Pravallikag123 <166217778+Pravallikag123@users.noreply.github.com>
2025-03-03 11:49:26 +0530

    server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const dotEnv = require('dotenv')
 const mongoose = require('mongoose')
-const bodyparser = require('body-parser')
-const itemRoutes = require('./routes/itemRoutes')
 const bodyParser = require('body-parser')
+const itemRoutes = require('./routes/itemRoutes')
+const serviceRoutes = require('./routes/serviceRoutes')
+const categoryRoutes = require('./routes/categoryRoutes')
+const brandRoutes = require('./routes/brandRoutes')
+const variantRoutes = require('./routes/variantRoutes')
+const importRoutes = require('./routes/importRoutes')
 
 const app = express()
-
 const PORT = process.env.PORT || 3000
 
 dotEnv.config()
@@ -14,15 +17,20 @@ dotEnv.config()
 app.use(bodyParser.json())
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
+  .then(() => {
     console.log('MongoDB connected successfully!')
-})
-.catch((error)=>{
-    console.log('$(error')
-})
+  })
+  .catch((error) => {
+    console.log('$(error)')
+  })
 
-app.use('/item',itemRoutes)
-app.listen(PORT,()=>{
-    console.log(`server started and running at ${PORT}`);
+app.use('/item', itemRoutes)
+app.use('/service', serviceRoutes)
+app.use('/category', categoryRoutes)
+app.use('/brand', brandRoutes)
+app.use('/variant', variantRoutes)
+app.use('/import', importRoutes)
 
+app.listen(PORT, () => {
+  console.log(`server started and running at ${PORT}`)
 })

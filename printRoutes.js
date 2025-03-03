f3ed0c1dacdb323c1280139beca27ae5d1ae4e64
Pravallikag123 <166217778+Pravallikag123@users.noreply.github.com>
2025-03-03 11:52:32 +0530

    printRoutes.js

--- printRoutes.js
+++ printRoutes.js
@@ -1,8 +1,7 @@
-// routes/printRoutes.js
+
 const express = require('express')
 const router = express.Router()
 
-// Define routes here, for example:
 router.get('/print', (req, res) => {
   res.send('Print route')
 })

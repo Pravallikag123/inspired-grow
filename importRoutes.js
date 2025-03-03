102f86a3d5f0589f44870f9156d9310b9b2e76d4
Pravallikag123 <166217778+Pravallikag123@users.noreply.github.com>
2025-03-03 11:52:00 +0530

    importRoutes.js

--- importRoutes.js
+++ importRoutes.js
@@ -1,8 +1,7 @@
-// routes/importRoutes.js
+
 const express = require('express')
 const router = express.Router()
 
-// Define routes here, for example:
 router.post('/import', (req, res) => {
   res.send('Import route')
 })

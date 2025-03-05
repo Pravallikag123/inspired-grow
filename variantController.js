89707f4c3e20e77b44f35fff4fd15a1a3d14b110
Pravallikag123 <166217778+Pravallikag123@users.noreply.github.com>
2025-03-05 13:57:47 +0530

    variantController.js

--- variantController.js
+++ variantController.js
@@ -2,18 +2,38 @@ const Variant = require('../models/Variant')
 
 const createVariant = async (req, res) => {
   try {
-    const { name, productId, price } = req.body
+    const {
+      name,
+      productId,
+      price,
+      tax,
+      purchasePrice,
+      taxType,
+      profitMargin,
+      salesPrice,
+      mrp,
+      expiryDate
+    } = req.body
 
+    
     const newVariant = new Variant({
       name,
       productId,
-      price
+      price,
+      tax,
+      purchasePrice,
+      taxType,
+      profitMargin,
+      salesPrice,
+      mrp,
+      expiryDate
     })
 
+  
     await newVariant.save()
     res.status(201).json(newVariant)
   } catch (error) {
-    console.error('Error:', error)
+    console.error('Error creating variant:', error)
     res.status(500).json({ message: 'Server error' })
   }
 }
@@ -21,12 +41,11 @@ const createVariant = async (req, res) => {
 const getVariants = async (req, res) => {
   try {
     const variants = await Variant.find().populate('productId')
-    res.status(200).json(variants)
+    res.status(200).json(variants);
   } catch (error) {
-    console.error('Error:', error)
+    console.error('Error fetching variants:', error)
     res.status(500).json({ message: 'Server error' })
   }
 }
-
 module.exports = { createVariant, getVariants }
 
